2cb3fe4a72ed35be2e6f294da27f9c6a799a51dc
Ben Walker <bwalker@asheavenue.com>
2014-04-02 21:25:59 -0400

    clean up

--- app/protected/views/admin/image/create.php
+++
@@ -1,5 +0,0 @@
-<div class="header">
-    <h1><?php echo $this->title; ?></h1>
-</div>
-
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
--- app/themes/admin/views/admin/site/index.php
+++
@@ -1,8 +0,0 @@
-<div class="header">
-    <h1><?php echo Yii::app()->name; ?> Dashboard</h1>
-</div>
-
-<div class="inner-content">
-   <p>Welcome, <?php echo Yii::app()->user->first_name; ?> <?php echo Yii::app()->user->last_name; ?>, to your website's admin section!</p>
-   <p>More to come...</p>
-</div>
