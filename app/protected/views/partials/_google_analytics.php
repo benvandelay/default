778365bf8aa07ef80103393c4b4d17af79f45f2d
Ben Walker <bwalker@asheavenue.com>
2014-06-20 15:31:02 -0400

    qf

--- app/protected/views/partials/_google_analytics.php
+++ app/protected/views/partials/_google_analytics.php
@@ -1,15 +1,12 @@
 <?php if(SiteHelper::getParam('google_tracking_id')): ?>
     
-    <?php $parts = explode('.', Yii::app()->request->serverName); ?>
-    <?php $sn    = $parts[1] . '.' . $parts[2]; ?>
-    
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', '<?php echo SiteHelper::getParam('google_tracking_id'); ?>', '<?php echo $sn; ?>');
+  ga('create', '<?php echo SiteHelper::getParam('google_tracking_id'); ?>', '<?php echo Yii::app()->request->serverName; ?>');
   ga('send', 'pageview');
 
 </script>
